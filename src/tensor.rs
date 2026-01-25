a7f79524b3dd67b4bf6eabe4444b3af46d9f2624
Miguevrgo <miguevrgo@gmail.com>
2026-01-25 22:57:18 +0100

    Upload {ones, xavier} initialization | Upload helper methods

--- src/tensor.rs
+++ src/tensor.rs
@@ -19,6 +19,14 @@ impl Tensor {
         }
     }
 
+    pub fn ones(shape: &[usize]) -> Self {
+        let size = shape.iter().product();
+        Self {
+            data: vec![1.0; size],
+            shape: shape.to_vec(),
+        }
+    }
+
     pub fn he(shape: &[usize]) -> Self {
         let size = shape.iter().product();
         let fan_in: usize = shape.iter().skip(1).product();
@@ -30,6 +38,48 @@ impl Tensor {
         Tensor {
             data,
             shape: shape.to_vec(),
+        }
+    }
+
+    pub fn xavier(shape: &[usize]) -> Self {
+        let size = shape.iter().product();
+        let fan_in = if shape.len() >= 2 {
+            shape[shape.len() - 1]
+        } else {
+            1
+        };
+        let fan_out = if shape.len() >= 2 {
+            shape[shape.len() - 2]
+        } else {
+            1
+        };
+        let std_deviation = (2.0 / (fan_in + fan_out) as f32).sqrt();
+        let normal_distr = Normal::new(0.0, std_deviation).unwrap();
+        let mut rng = rand::rng();
+        let data: Vec<f32> = (0..size).map(|_| normal_distr.sample(&mut rng)).collect();
+
+        Tensor {
+            data,
+            shape: shape.to_vec(),
+        }
+    }
+
+    pub fn reshape(&self, new_shape: &[usize]) -> Result<Tensor, &'static str> {
+        let n_size = new_shape.iter().product();
+        if self.data.len() != n_size {
+            return Err("New shape must have the same number of total elements");
+        }
+
+        Ok(Tensor {
+            data: self.data.clone(),
+            shape: new_shape.to_vec(),
+        })
+    }
+
+    pub fn flatten(&self) -> Tensor {
+        Tensor {
+            data: self.data.clone(),
+            shape: vec![self.data.len()],
         }
     }
 }
